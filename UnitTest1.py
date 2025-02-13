18015d1b3e55d77d89b06ade2dfe5bb6b579044f
Inferno <ianthewrong@gmail.com>
2025-02-13 18:40:00 -0500

    updated gitignore

--- UnitTest1.py
+++ UnitTest1.py
@@ -15,6 +15,21 @@ def populate(iterations : int, rangeLow : float, rangeHigh : float, clear : bool
                     data.write(writeData)
 
 def randFind(iterations : int, rangeLow : float, rangeHigh : float):
-     for i in range(iterations):
+     open("log.txt", "w").close()
+     with open("log.txt", "a") as data:
+        for i in range(iterations):
+            temp = [float(rand.randrange(rangeLow,rangeHigh)), float(rand.randrange(rangeLow,rangeHigh)), float(rand.randrange(rangeLow,rangeHigh))]
+            temp = sorted(temp)
+            output = boxes.find(temp[0],temp[1],temp[2])
+            boxes.remove(output[3])
+            if(output[0] == -1):
+                data.write("No Boxes For Specifications")
+            else:
+                data.write(str(output[0]) + " x " + str(output[1]) + " x " + str(output[2]) + " ORIGINAL: " + str(temp[0]) + " x " + str(temp[1]) + " x " + str(temp[2]) + "\n")
+populate(1000, 1, 300, True)
+randFind(300, 2, 100)
+
+
+
           
 
